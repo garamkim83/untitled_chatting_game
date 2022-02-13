efa4490be6521536f73629c1d47e8d0199e4c0a0
garamkim83 <garamk83@gmail.com>
2022-02-13 13:25:33 +0900

    just backup

--- lib/friendsList_design.dart
+++ lib/friendsList_design.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 
-class FriendsList extends StatelessWidget {
-  //const ({ Key? key }) : super(key: key);
+import './homepage_design.dart';
+
+class FriendTab extends StatefulWidget {
+  static const title = 'Friends';
+  static const androidIcon = Icon(Icons.account_circle_outlined);
+  static const iosIcon = Icon(CupertinoIcons.person_2);
+
+  const FriendTab({Key? key, this.androidDrawer}) : super(key: key);
+
+  final Widget? androidDrawer;
 
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  _FriendTabState createState() => _FriendTabState();
+}
+
+class _FriendTabState extends State<FriendTab> {
+  static const _itemsLength = 50;
+
+  final _androidRefreshKey = GlobalKey<RefreshIndicatorState>();
+
+  @override
+  void initState() {
+    _setData();
+    super.initState();
+  }
+
+  void _setData(){
+    colors = getRandomColors(_itemsLength)
+  }
+
+  Widget _listBuilder(BuildContext context, int index){
+    if() return Container();
   }
 }
